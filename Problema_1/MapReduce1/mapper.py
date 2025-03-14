ed9387f85be00306fe4c3280edf755b5d0357470
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-03-13 23:46:49 -0400

    Prueba basica de ejecucion del map reduce. Falta pulir la lectura y generacion de par clave valor para mejor recepcion por parte del reducer.

--- Problema_1/MapReduce1/mapper.py
+++ Problema_1/MapReduce1/mapper.py
@@ -1,15 +1,14 @@
-# Mapper para combinar datos de usuarios y ubicaciones
+#!/usr/bin/python3
 
 import sys
-import csv
 
+# Mapper para combinar datos de usuarios y ubicaciones
 def mapper():
-    reader = csv.reader(sys.stdin)
-    for row in reader:
-        if len(row) > 1:
-            user_id = row[0]
-            location = row[4]  # Assuming 'Location' is the 5th column in vshort-users-details-2023.csv
-            print(f"{user_id}\t{location}")
+	for line in sys.stdin:
+		splits = line.strip().split(",")
+		user_id = splits[0]
+		location = splits[4]  # Assuming 'Location' is the 5th column in vshort-users-details-2023.csv
+		print(f"{user_id} {location}")
 
 if __name__ == "__main__":
-    mapper()
+	mapper()
--- Problema_1/MapReduce1/reducer.py
+++ Problema_1/MapReduce1/reducer.py
@@ -1,18 +1,16 @@
-# Reducer para combinar datos de usuarios y ubicaciones
+#!/usr/bin/python3
 
 import sys
-from collections import defaultdict
 
+# Reducer para combinar datos de usuarios y ubicaciones
 def reducer():
-    user_locations = {}
-    
     for line in sys.stdin:
         line = line.strip()
-        user_id, location = line.split('\t')
-        user_locations[user_id] = location
-    
-    for user_id, location in user_locations.items():
-        print(f"{user_id}\t{location}")
-
+        splits = line.split(' ')
+        print(splits)
+        #user_id = splits[0]
+        #location = splits[1]
+        #print(f"{user_id}\t{location}")
+        
 if __name__ == "__main__":
     reducer()
