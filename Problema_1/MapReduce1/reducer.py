169efcbab4a3ee93df1f5c3f71513905e3620a2f
A6575 <astridaalvaradoa@gmail.com>
2025-03-15 10:45:15 -0400

    Modificacion del reducer para manejar la nueva entrada (salida del mapper). Logistica para agrupar users con su ubicacion

--- Problema_1/MapReduce1/reducer.py
+++ Problema_1/MapReduce1/reducer.py
@@ -4,13 +4,31 @@ import sys
 
 # Reducer para combinar datos de usuarios y ubicaciones
 def reducer():
+    current_user_id = None
+    current_location = None
+
     for line in sys.stdin:
-        line = line.strip()
-        splits = line.split(' ')
-        print(splits)
-        #user_id = splits[0]
-        #location = splits[1]
-        #print(f"{user_id}\t{location}")
+        parts = line.strip().split("\t")
         
+        if len(parts) != 2:
+            continue # Si no hay dos partes, se ignora la línea (i.e no hay ubicación)
+
+        user_id, location = parts 
+        
+        # Si el usuario es el mismo, se ignora
+        if current_user_id == user_id:
+            continue
+        else:
+            # Si el usuario cambia, se imprime el usuario y su ubicación
+            if current_user_id:
+                print(f"{current_user_id}\t{current_location}")
+            # Se actualiza el usuario y la ubicación
+            current_user_id = user_id
+            current_location = location
+
+    # Se imprime el último usuario y su ubicación
+    if current_user_id == user_id:
+        print(f"{current_user_id}\t{current_location}")
+
 if __name__ == "__main__":
     reducer()
