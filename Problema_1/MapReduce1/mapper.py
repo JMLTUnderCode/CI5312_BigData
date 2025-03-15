c89cb1faa5f9333f62a5054971218cdcb8dde81e
A6575 <astridaalvaradoa@gmail.com>
2025-03-15 10:44:18 -0400

    Modificacion del mapper para separar la entrada por \t en lugar de espacio en blanco

--- Problema_1/MapReduce1/mapper.py
+++ Problema_1/MapReduce1/mapper.py
@@ -8,7 +8,7 @@ def mapper():
 		splits = line.strip().split(",")
 		user_id = splits[0]
 		location = splits[4]  # Assuming 'Location' is the 5th column in vshort-users-details-2023.csv
-		print(f"{user_id} {location}")
+		print(f"{user_id}\t{location}") # Se separan los datos por tabulador para mejor manejo en el reducer
 
 if __name__ == "__main__":
 	mapper()
