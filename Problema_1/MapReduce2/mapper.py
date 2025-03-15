ba868505ecaaf795127b657166c3488063fc1547
A6575 <astridaalvaradoa@gmail.com>
2025-03-15 12:05:02 -0400

    Inclusion de logistica para obtener los usuarios y puntuaciones en el mapper

--- Problema_1/MapReduce2/mapper.py
+++ Problema_1/MapReduce2/mapper.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 
 import sys
-import csv
 
 # Mapper para combinar datos de usuarios y sus puntuaciones
 def mapper():
-    reader = csv.reader(sys.stdin)
-    for row in reader:
-        if len(row) > 1:
-            user_id = row[0]
-            rating = row[4]  # Assuming 'rating' is the 5th column in vshort-users-score-2023.csv
-            print(f"{user_id}\t{rating}")
+    for line in sys.stdin:
+        splits = line.strip().split(",")
+        user_id = splits[0]
+        score = splits[4]  # Assuming 'Score' is the 3rd column in vshort-users-score-2023.csv
+        print(f"{user_id}\t{score}")
 
 if __name__ == "__main__":
     mapper()
