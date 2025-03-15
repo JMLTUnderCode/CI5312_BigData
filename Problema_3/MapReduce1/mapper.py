0f5deeca7aadff7ff8943b5ab02f5359e3320cd1
A6575 <astridaalvaradoa@gmail.com>
2025-03-15 16:57:13 -0400

    Modificacion del mapper del Problema 3 para usar libreria csv

--- Problema_3/MapReduce1/mapper.py
+++ Problema_3/MapReduce1/mapper.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
 
+"""
+	Este Mapper esta encargado de recibir la información del archivo 
+    "final_animedataset.csv" y realizar la transformación de la información
+    tomando dos columnas importantes 
+    - En csv "gender" = En codigo "user_gender"
+    - En csv "genre" = En codigo "genres"
+    
+    Posteriormente se realiza un split de la columna "genre" para obtener
+    los generos asociados de cada anime y se imprime el par genero_anime-genero_usuario.
+    
+    Colaboradores en este archivo:
+    - Astrid Alvarado
+"""
+
 import sys
+import csv
 
 def mapper():
-    next(sys.stdin)  # Skip header
-    for line in sys.stdin:
-        fields = line.strip().split(',')
-        if len(fields) > 12:
-            user_gender = fields[4]  # Assuming 'gender' is the 5th column
-            genres = fields[12].strip('"')  # Assuming 'genre' is the 13th column
+    reader = csv.reader(sys.stdin)
+    next(reader)  # Se omite la cabecera
+    for row in reader:
+        if len(row) > 12:
+            user_gender = row[4]  # Columna 'gender'
+            genres = row[12]  # Columna 'genre'
+
             for genre in genres.split(','):
-                print(f"{genre.strip()}\t{user_gender}")
+                genre = genre.strip()   # Se elimina espacios en blanco
+                if genre:
+                    print(f"{genre}\t{user_gender}")
 
 if __name__ == "__main__":
     mapper()
