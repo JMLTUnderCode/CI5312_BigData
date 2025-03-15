58f98c2d8ef97e485b6c1f5dd5daa721b39d7578
A6575 <astridaalvaradoa@gmail.com>
2025-03-15 10:54:20 -0400

    Modificacion del mapper para limpiar los datos de ubicacion

--- Problema_1/MapReduce1/mapper.py
+++ Problema_1/MapReduce1/mapper.py
@@ -7,7 +7,7 @@ def mapper():
 	for line in sys.stdin:
 		splits = line.strip().split(",")
 		user_id = splits[0]
-		location = splits[4]  # Assuming 'Location' is the 5th column in vshort-users-details-2023.csv
+		location = splits[4].strip('"')  # Assuming 'Location' is the 5th column in vshort-users-details-2023.csv
 		print(f"{user_id}\t{location}") # Se separan los datos por tabulador para mejor manejo en el reducer
 
 if __name__ == "__main__":
