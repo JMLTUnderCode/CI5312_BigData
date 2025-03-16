253c9038d6394ae086f3c8e45715581928775f91
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-03-16 00:42:53 -0400

    Creacion base de archivos para item 3 de problema 3

---
+++ Problema_3/Item_3/mapper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+	Este Mapper esta encargado de recibir la información del archivo 
+    ".csv"
+    Colaboradores en este archivo:
+    - Astrid Alvarado
+    - Junior Lara
+"""
+
+# POR CODEAR
---
+++ Problema_3/Item_3/reducer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+	Este Reducer esta encargado de recibir los datos del mapper asociado
+    que constituye 
+    
+    Colaboradores en este archivo:
+    - Junior Lara
+"""
+
+# POR CODEAR
